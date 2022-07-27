aa78b88557653228f154df982a8c5884e175a04d
Halil Olcay <holcay92@gmail.com>
2022-07-27 17:53:23 +0300

    footer design editted columns are added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -65,7 +65,7 @@ function Footer() {
                         </div>
 
                         <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
-                            <h6 className='text-uppercase fw-bold mb-4'>Oteller</h6>
+                            <h6 className='Oteller'>Oteller</h6>
                             <p>
                                 <a href='www.setur.com.tr' className='text-reset'>
                                     Belek Otelleri
@@ -99,7 +99,73 @@ function Footer() {
                         </div>
 
                         <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
-                            <h6 className='text-uppercase fw-bold mb-4'>Otel Temaları</h6>
+                            <h6 className='Oteller'>Otel Temaları</h6>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Erken Rezervasyon Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Kıbrıs Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Balayı Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                   Son Dakika Tatil Fırsatları
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                   Bayram Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                   Tüm Otel Temaları <img src={rightArrow} alt=''/>
+                                </a>
+                            </p>
+                        </div>
+                        <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+                            <h6 className='Oteller'>Turlar</h6>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Erken Rezervasyon Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Kıbrıs Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                    Balayı Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                   Son Dakika Tatil Fırsatları
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                   Bayram Otelleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='#!' className='text-reset'>
+                                   Tüm Otel Temaları <img src={rightArrow} alt=''/>
+                                </a>
+                            </p>
+                        </div>
+                        <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+                            <h6 className='Oteller'>Diğer Hizmetlerimiz</h6>
                             <p>
                                 <a href='#!' className='text-reset'>
                                     Erken Rezervasyon Otelleri
@@ -132,8 +198,10 @@ function Footer() {
                             </p>
                         </div>
 
+
+
                         <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
-                            <h6 className='text-uppercase fw-bold mb-4'>Turlar</h6>
+                            <h6 className='Oteller'>İletişim</h6>
                             <p>
                                 <i className='fas fa-home me-3'></i> Istanbul, 34400, Türkiye
                             </p>

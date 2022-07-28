477640061f62b1db121e6ea759d393b838f067bb
Halil Olcay <holcay92@gmail.com>
2022-07-28 10:50:45 +0300

    footer urls editted, whatsapp  div added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MDBFooter } from 'mdb-react-ui-kit';
+import Card from 'react-bootstrap/Card';
 import facebookIcon from './images/footerImages/facebook.svg';
 import InstagramIcon from './images/footerImages/instagram.svg';
 import TwitterIcon from './images/footerImages/twitter.svg';
@@ -10,6 +11,7 @@ import appledownload from './images/footerImages/appledownload.png';
 import androiddownload from './images/footerImages/androiddownload.png';
 import rightArrow from './images/footerImages/rightarrowicon.svg';
 
+
 function Footer() {
     return (
 
@@ -17,31 +19,31 @@ function Footer() {
             <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
 
                 <div className='socialmedia' style={{ backgroundColor: 'white' }} >
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.facebook.com.tr' className='me-4 text-reset'>
                         <img className='facebookicon' src={facebookIcon} alt='' />
                         <i className='fab fa-facebook-f'></i>
                     </a>
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.twitter.com.tr' className='me-4 text-reset'>
                         <img className='facebookicon' src={TwitterIcon} alt='' />
                         <i className='fab fa-twitter'></i>
                     </a>
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.pinterest.com.tr' className='me-4 text-reset'>
                         <img className='facebookicon' src={PinterestIcon} alt='' />
                         <i className='fab fa-instagram'></i>
                     </a>
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.instagram.com.tr' className='me-4 text-reset'>
                         <img className='facebookicon' src={InstagramIcon} alt='' />
                         <i className='fab fa-instagram'></i>
                     </a>
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.youtube.com.tr' className='me-4 text-reset'>
                         <img className='facebookicon' src={YoutubeIcon} alt='' />
                         <i className='fab fa-linkedin'></i>
                     </a>
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.apple.com.tr' className='me-4 text-reset'>
                         <img className='appledownload' src={appledownload} alt='' />
                         <i className='fab fa-linkedin'></i>
                     </a>
-                    <a href='www.setur.com.tr' className='me-4 text-reset'>
+                    <a href='https://www.google.com.tr' className='me-4 text-reset'>
                         <img className='androiddownload' src={androiddownload} alt='' />
                         <i className='fab fa-linkedin'></i>
                     </a>
@@ -50,50 +52,96 @@ function Footer() {
             </section>
 
             <section className=''>
+                
+                    <Card className='whatsappCard'>
+                    <Card.Body className='whatsappcardbody' >
+                        <div className='Whatsapp' >
+                            <h3 className='Whatsapp'>WhatsApp
+                                <p className='WhatsappNumber'>
+                                    0579 804 67 00 </p></h3>
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Card className='whatsappCard'>
+                    <Card.Body className='whatsappcardbody' >
+                        <div className='Whatsapp' >
+                            <h3 className='Whatsapp'>WhatsApp
+                                <p className='WhatsappNumber'>
+                                    0579 804 67 00 </p></h3>
+                        </div>
+                    </Card.Body>
+                </Card>
+                
+                
+
                 <div className='container text-center text-md-start mt-5'>
                     <div className='row mt-3'>
                         <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
                             <h6 className='text-uppercase fw-bold mb-4'>
                                 <img className='seturfootericon' src={seturfootericon} alt='' />
-
-
                             </h6>
                             <p>
-                                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                                consectetur adipisicing elit.
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Hakkımızda
+                                </a>
+                            </p>
+                            <p>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Ödeme Seçenekleri
+                                </a>
+                            </p>
+                            <p>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Hediye Kartı
+                                </a>
+                            </p>
+                            <p>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Acenta Başvuru Formu
+                                </a>
+                            </p>
+                            <p>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Kampanyalar
+                                </a>
+                            </p>
+                            <p>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Broşürler
+                                </a>
                             </p>
                         </div>
 
                         <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
                             <h6 className='Oteller'>Oteller</h6>
                             <p>
-                                <a href='www.setur.com.tr' className='text-reset'>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
                                     Belek Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='www.setur.com.tr' className='text-reset'>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
                                     Kıbrıs Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='www.setur.com.tr' className='text-reset'>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
                                     Side Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='www.setur.com.tr' className='text-reset'>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
                                     Marmaris Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='www.setur.com.tr' className='text-reset'>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
                                     Çeşme Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='www.setur.com.tr' className='text-reset'>
-                                    Tüm Oteller <img src={rightArrow} alt=''/>
+                                <a href='www.setur.com.tr' className='footerSubtitleColor'>
+                                    Tüm Oteller <img className='rightarrowicon' src={rightArrow} alt='' />
                                 </a>
                             </p>
                         </div>
@@ -101,119 +149,119 @@ function Footer() {
                         <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
                             <h6 className='Oteller'>Otel Temaları</h6>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Erken Rezervasyon Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Kıbrıs Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Balayı Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Son Dakika Tatil Fırsatları
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Son Dakika Tatil Fırsatları
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Bayram Otelleri
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Bayram Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Tüm Otel Temaları <img src={rightArrow} alt=''/>
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Tüm Otel Temaları <img className='rightarrowicon' src={rightArrow} alt='' />
                                 </a>
                             </p>
                         </div>
                         <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
                             <h6 className='Oteller'>Turlar</h6>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Erken Rezervasyon Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Kıbrıs Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Balayı Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Son Dakika Tatil Fırsatları
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Son Dakika Tatil Fırsatları
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Bayram Otelleri
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Bayram Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Tüm Otel Temaları <img src={rightArrow} alt=''/>
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Tüm Otel Temaları <img className='rightarrowicon' src={rightArrow} alt='' />
                                 </a>
                             </p>
                         </div>
                         <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
                             <h6 className='Oteller'>Diğer Hizmetlerimiz</h6>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Erken Rezervasyon Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Kıbrıs Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
+                                <a href='#!' className='footerSubtitleColor'>
                                     Balayı Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Son Dakika Tatil Fırsatları
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Son Dakika Tatil Fırsatları
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Bayram Otelleri
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Bayram Otelleri
                                 </a>
                             </p>
                             <p>
-                                <a href='#!' className='text-reset'>
-                                   Tüm Otel Temaları <img src={rightArrow} alt=''/>
+                                <a href='#!' className='footerSubtitleColor'>
+                                    Tüm Otel Temaları <img className='rightarrowicon' src={rightArrow} alt='' />
                                 </a>
                             </p>
                         </div>
 
 
 
-                        <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+                        <div className='footerSubtitleColor'>
                             <h6 className='Oteller'>İletişim</h6>
                             <p>
-                                <i className='fas fa-home me-3'></i> Istanbul, 34400, Türkiye
+                                <i className='footerSubtitleColor'></i> Istanbul, 34400, Türkiye
                             </p>
                             <p>
-                                <i className='fas fa-envelope me-3'></i>
+                                <i className='footerSubtitleColor'></i>
                                 info@setur.com.tr
                             </p>
                             <p>
-                                <i className='fas fa-phone me-3'></i> + 90 216 xxx xx xx
+                                <i className='footerSubtitleColor'></i> + 90 216 xxx xx xx
                             </p>
                             <p>
-                                <i className='fas fa-print me-3'></i> + 90 216 xxx xx xx
+                                <i className='footerSubtitleColor'></i> + 90 216 xxx xx xx
                             </p>
                         </div>
                     </div>
@@ -221,7 +269,7 @@ function Footer() {
             </section>
 
             <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-                © 2022 Copyright:
+                {"© 2022 Copyright : "}
                 <a className='text-reset fw-bold' href='www.setur.com.tr'>
                     www.setur.com.tr
                 </a>
